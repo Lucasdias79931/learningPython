7d1687a97ba5ac4b49f6ab62d259b3a4d344bc16
Lucasdias79931 <lucasdias26@hotmail.com.br>
2025-02-06 00:19:24 -0300

    atualizado

--- hashInflie/hash.py
+++ hashInflie/hash.py
@@ -1,25 +1,53 @@
 import hashlib
 import os
+import re
 
 def insertHash(password: str) -> str:
     hashstr = hashlib.sha512(password.encode()).hexdigest()
     return hashstr
 
-here = os.getcwd()
+def writeData(outfile, data):
+    try:
+        with open(outfile, "a") as file:
+            for key, value in data.items():
+                file.write(f"{key}:{value}\n")
+        print("Dados do usuário inseridos com sucesso!")
+    except FileExistsError as e:
+        print(e)
+    except Exception as e:
+        print(e)
 
-pessoas = os.path.join(here, "Pessoas.txt")
+def autentica(inputfile, passw):
+    try:
+        with open(inputfile, "r") as file:
+            passw = hashlib.sha512(passw.encode()).hexdigest()
+            dataAux = {}
+            for line in file:
+                key, value = line.strip().split(":")
+                
+                dataAux[key] = value
+               
+                if key == "Password":
+                    if value == passw:
+                        return dataAux
+                    else:
+                        dataAux.clear()
 
-try:
-    
-    email = input("Digite o email:")
-    password = input("Digite a senha:")
-    password = insertHash(password)
+            return False
+            
+    except FileExistsError as e:
+        print(e)
+    except Exception as e:
+        print(e)
 
-    
-    with open(pessoas, "a") as file:
-        file.write(f"Email: {email}\nPassword: {password}\n")
-    print("Dados do usuário inseridos com sucesso!")
-except FileExistsError as e:
-    print(e)
-except Exception as e:
-    print(e)
+data = autentica("Pessoas.txt", "gatocezar")
+
+if data: 
+    print("Autenticação bem sucedida")
+
+    for key, value in data.items():
+        print(f"{key}:{value}")
+else:
+    print("Pessoa não encontrada!")
+
+
--- strings/main.py
+++ strings/main.py
@@ -1,8 +1,45 @@
-string = "lucas dos lucas dos lucas dos lucas dos santos lucas dos lucas dos santos dias santos dias dias santos dias santos dias santos dias"
 
-sub = string.split(" ")
-ocorrencia = string.count("dos")
+def strStr(haystack: str, needle: str) -> int:
+        if not needle or not haystack:
+            return -1
+        if len(needle) > len(haystack):
+            return -1
 
-mod = "-".join(string.replace(" ", "")).split("-")
+        sizeNeedle = len(needle)
+        
+        end = sizeNeedle 
+        
+        for start in range(len(haystack)):
+            
+            word = haystack[start:end]
+            print(end)
+            if word == needle:
+                return start
+            
+            end +=1
+        return -1
+            
+def isPalindrome(s:str)->bool:
+    return s == s[::-1]
 
-print(mod)
+
+
+class Solution:
+    def __init__(self):
+        pass
+
+    def isPalindrome(self, s):
+        return s == s[::-1]
+
+    def longestPalindrome(self, s: str) -> str:
+        longest_pali = ""
+
+        for start in range(len(s)):
+            for end in range(start + 1, len(s) + 1):
+                word = s[start:end]
+                if self.isPalindrome(word) and len(word) > len(longest_pali):
+                    longest_pali = word
+
+        return longest_pali if longest_pali else s[0]  
+
+
