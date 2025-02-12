bb5d6f42dbc1a7f6a97cf55534883a5c69ce9ef0
Lucasdias79931 <lucasdias26@hotmail.com.br>
2025-02-12 01:27:56 -0300

    listcomprehension

--- listcomprehension/main.py
+++ listcomprehension/main.py
@@ -1,15 +1,34 @@
+
+def isprimo(number):
+    if number < 2:
+        return False
+    if number == 2:
+        return True  
+
+    if number % 2 == 0:
+        return False  
+
+    for i in range(3, number, 1):  
+        if number % i == 0:
+            return False  
+
+    return True
+
+
 lista = [numero for numero in range(10)]
+
 
 lista2 = [n for n in lista if n%2 == 0]
 lista3 = [n for n in lista if n%2 == 1]
-lista2.reverse()
 
-lista = []
-for i in range((len(lista2)+len(lista3))//2):
-    lista.append(lista2.pop())
-    lista.append(lista3.pop())
+primos = [n for n in lista if isprimo(n)]
 
-print(lista)
+primosImp = [n for n in lista if isprimo(n) and n%2 ==1]
+primosPar = [n for n in lista if isprimo(n) and n%2 ==0]
+print(f"somente par:{lista2}\nsomente impar:{lista3}")
 
-    
+print("primos:", primos)
+print(f"primos pares:{primosPar}\nprimos Imp:{primosImp}")
+
+
 
